2a991609d1fb61bce5adbcc7f00f2424eb760501
franklinselva <franklinselva10@gmail.com>
2024-08-08 09:52:16 +0200

    Remove Earthifile builds

--- run.sh
+++ run.sh
@@ -8,8 +8,8 @@
 IMG_NAME=osrf/space-ros
 # Replace `/` with `_` to comply with docker container naming
 # And append `_runtime`
-CONTAINER_NAME="$(tr '/' '_' <<< "$IMG_NAME")"
+CONTAINER_NAME="$(tr '/' '_' <<<"$IMG_NAME")"
 
 # Start the container
-docker run --rm -it --name $CONTAINER_NAME  --network host \
-    -e DISPLAY -e TERM   -e QT_X11_NO_MITSHM=1 $IMG_NAME
+docker run --rm -it --name "$CONTAINER_NAME" --network host \
+    -e DISPLAY -e TERM -e QT_X11_NO_MITSHM=1 $IMG_NAME
